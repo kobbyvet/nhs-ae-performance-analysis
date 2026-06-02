9cc9db103854ebfd6a9805e3f5e6032bf3cde0b3
kobbyvet <160423217+kobbyvet@users.noreply.github.com>
2026-06-02 14:45:35 +0100

    Delete emergency_trend.sql

--- emergency_trend.sql
+++
@@ -1,9 +0,0 @@
---Emergency Admission Trend
-
-SELECT
-    period,
-    SUM(total_emergency_admissions) AS total_emergency_admissions
-FROM ae_data
-WHERE total_emergency_admissions IS NOT NULL
-GROUP BY period
-ORDER BY period;
